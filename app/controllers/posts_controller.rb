1079b5060aab14ee96e32330c563c79441884f17
上田洸太 <uedakota@uedakoutanoMacBook-puro.local>
2019-07-06 01:39:20 +0900

    新規投稿フォームをindexページで表示する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
+  before_action :post_params, only: [:create]
+
   def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index', to: 'posts#index'
+  post "posts" => "posts#create"
 end
